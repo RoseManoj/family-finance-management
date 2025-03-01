cb7b08e1f97e20438f5ae8fbbd1a1832f0136091
Roshan P J <roshanpj10@gmail.com>
2025-03-01 16:55:23 +0530

    initial

---
+++ src/main/java/com/example/family_finance_management/FamilyFinanceManagementApplication.java
@@ -0,0 +1,21 @@
+package com.example.family_finance_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FamilyFinanceManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyFinanceManagementApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")  // <-- This maps to "http://localhost:8080/"
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
