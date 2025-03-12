d222800b12e529314c5f334463d1eb53a2e0da80
rinto11804 <rinto11804@gmail.com>
2025-03-12 20:24:53 +0530

    added mysql database connection

--- src/main/java/com/example/family_finance_management/FamilyFinanceManagementApplication.java
+++ src/main/java/com/example/family_finance_management/FamilyFinanceManagementApplication.java
@@ -4,18 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 public class FamilyFinanceManagementApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FamilyFinanceManagementApplication.class, args);
     }
 }
 
+record Greeting(Long id, String content, boolean error) {
+}
+
 @RestController
 class HelloController {
-    @GetMapping("/")  // <-- This maps to "http://localhost:8080/"
-    public String sayHello() {
-        return "Hellooo!";
+
+    public final AtomicLong count = new AtomicLong();
+
+    @GetMapping("/") // <-- This maps to "http://localhost:8080/"
+    public Greeting sayHello() {
+        return new Greeting(count.incrementAndGet(), "Hello World", false);
     }
 }
---
+++ src/main/java/com/example/family_finance_management/user/User.java
@@ -0,0 +1,49 @@
+package com.example.family_finance_management.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String email;
+
+    // Add default constructor for JPA
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Add id getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
