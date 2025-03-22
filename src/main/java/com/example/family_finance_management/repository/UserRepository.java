4404e3236d18a9a1071924b010a7dc1e5b593592
Roshan P J <roshanpj10@gmail.com>
2025-03-22 21:49:20 +0530

    auth set

--- src/main/java/com/example/family_finance_management/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.family_finance_management.repository;
-
-import com.example.family_finance_management.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
